f14da233801f4aab063690f2b9084cb20deac049
Sunny Juneja <me@sunnyjuneja.com>
2015-01-30 10:20:26 -0800

    Integrate recaptcha.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,14 @@
 class AnimalsController < ApplicationController
   def new
+    @animal = Animal.new
+  end
+
+  def create
+    if verify_recaptcha
+      render 'show'
+    else
+      @animal = Animal.new
+      render 'new'
+    end
   end
 end
